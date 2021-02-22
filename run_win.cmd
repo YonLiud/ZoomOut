dfdc190746ebb6fc64632dd125779888f77c0d93
=YonLiud <=ejliud@gmail.com>
2021-02-22 14:45:10 +0200

    Update 2

--- run_win.cmd
+++ run_win.cmd
@@ -9,14 +9,28 @@ where py >nul 2>&1 && goto boot || goto exit
     echo [91mPython Is Not Installed[0m
     pause
     Exit /B 5
-:boot
-    echo [102;90mPython is Installed![0m
-    echo [93mInstalling Lib...[0m
-    pip3 install win10toast
-    pip3 install flask
+
+:startup
+    pip3 install win10toast >nul 2>&1
+    pip3 install flask >nul 2>&1
     echo [92mDone![0m
     echo [93mStarting Browser...[0m
     start http://localhost:5000
     echo [104;97mStarting ZoomOut![0m
     py app.py
     echo [104;97mApp has been closed[0m
+    exit
+
+:boot
+    echo [102;90mPython is Installed![0m
+    echo [93mInstalling Lib...[0m
+    pip3 install virtualenv >nul 2>&1
+    if exist venv/ (
+        echo [92mvenv already exists[0m
+    ) else (
+        echo [93mCreating virtual enviroment[0m
+        python -m virtualenv venv
+    )
+    echo [104;97mStarting venv[0m
+    CALL venv\Scripts\activate.bat
+    goto startup
